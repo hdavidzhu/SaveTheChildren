2ac942af9ef98d52842938841132c369d3eae7e4
H. David Zhu <huanzhendavidzhu@gmail.com>
2014-11-08 14:36:59 -0500

    Simplified XML

--- app/src/main/java/com/hdavidzhu/savethechildren/Form.java
+++ app/src/main/java/com/hdavidzhu/savethechildren/Form.java
@@ -8,47 +8,22 @@ import org.json.JSONObject;
 public class Form {
 
     String name;
-    String starttime;
-    String startdate;
-    String endtime;
-    String enddate;
+    String timestamp;
     String notes;
 
-    public Form(){}
-
-    public Form(String name, String starttime, String startdate, String endtime, String enddate, String notes){
+    public Form(String name, String timestamp, String notes){
         this.name = name;
-        this.starttime = starttime;
-        this.startdate = startdate;
-        this.endtime = endtime;
-        this.enddate = enddate;
+        this.timestamp = timestamp;
         this.notes = notes;
     }
-
-    public void setName(String name) {this.name = name;}
-    public void setStarttime(String starttime) {this.starttime= starttime;}
-    public void setStartdate(String startdate) {this.startdate = startdate;}
-    public void setEndtime(String endtime) {this.endtime = endtime;}
-    public void setEnddate(String enddate) {this.enddate = enddate;}
-    public void setNotes(String notes) {this.notes = notes;}
-
-
-    public String getName() {return name;}
-    public String getStarttime() {return starttime;}
-    public String getStartdate() {return startdate;}
-    public String getEndtime() {return endtime;}
-    public String getEnddate() {return enddate;}
-    public String getNotes() {return notes;}
 
     public JSONObject javaToJSONObjectConverter (){
         JSONObject jsonForm = new JSONObject();
         try {
             jsonForm.put("name", this.name);
-//            jsonForm.put("starttime", this.starttime);
-//            jsonForm.put("startdate", this.startdate);
-//            jsonForm.put("endtime", this.endtime);
-//            jsonForm.put("enddate", this.enddate);
-//            jsonForm.put("notes", this.notes);
+            jsonForm.put("timestamp", this.timestamp);
+            jsonForm.put("notes", this.notes);
+
         }
         catch (JSONException e) {
             Log.d("Some tag", Log.getStackTraceString(e.getCause().getCause()));
--- app/src/main/java/com/hdavidzhu/savethechildren/RecordTutorSessionActivity.java
+++ app/src/main/java/com/hdavidzhu/savethechildren/RecordTutorSessionActivity.java
@@ -1,29 +1,21 @@
 package com.hdavidzhu.savethechildren;
 
 import android.app.Activity;
-import android.app.DownloadManager;
 import android.content.Context;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
-import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
-import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
 import org.json.JSONObject;
-
-import java.util.HashMap;
-import java.util.Map;
 
 public class RecordTutorSessionActivity extends Activity {
 
@@ -38,8 +30,6 @@ public class RecordTutorSessionActivity extends Activity {
     Button submitButton;
 
     View.OnClickListener submitButtonListener;
-
-    JSONObject jsonForm;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -68,9 +58,9 @@ public class RecordTutorSessionActivity extends Activity {
                         notesEditText.getText().toString()
                 );
 
-                jsonForm = form.javaToJSONObjectConverter();
+                JSONObject jsonForm = form.javaToJSONObjectConverter();
                 Log.d("something","something");
-                postNewForm(context, form);
+                postNewForm(context, jsonForm);
                 Log.d("something","something2");
 
             }
@@ -80,18 +70,16 @@ public class RecordTutorSessionActivity extends Activity {
 
     }
 
-    @Override
-    protected void onStart() {
-        super.onStart();
-
-    }
-
-    public void postNewForm(Context context, final Form form){
+    public void postNewForm(Context context, JSONObject jsonForm){
         // Instantiate the RequestQueue.
         final RequestQueue queue = Volley.newRequestQueue(context);
         String url = "http://192.168.56.101:3000/api/tests";
 
-        final JsonObjectRequest jsonRequest = new JsonObjectRequest(Request.Method.POST, url, jsonForm, new Response.Listener<JSONObject>() {
+        final JsonObjectRequest jsonRequest = new JsonObjectRequest(
+                Request.Method.POST,
+                url,
+                jsonForm,
+                new Response.Listener<JSONObject>() {
 
             @Override
             public void onResponse(JSONObject response) {
@@ -105,55 +93,10 @@ public class RecordTutorSessionActivity extends Activity {
             }
         });
 
-        // Posting queue.
-//        final StringRequest stringRequest = new StringRequest(Request.Method.POST, url, new Response.Listener<String>() {
-//            @Override
-//            public void onResponse(String response) {
-//                Log.d("onResponse", "Success");
-//            }
-//        }, new Response.ErrorListener() {
-//            @Override
-//            public void onErrorResponse(VolleyError error) {
-//                System.out.print("David stinks. just kidding");
-//                Log.d("This is Log.d", "David stinks. just kidding");
-//            }
-//        }){
-//            @Override
-//            protected Map<String, String> getParams () throws AuthFailureError{
-//                Map<String, String> formMap = new HashMap<String, String>();
-//                formMap.put("name", form.name);
-//                formMap.put("starttime", form.starttime);
-//                formMap.put("startdate", form.startdate);
-//                formMap.put("endtime", form.endtime);
-//                formMap.put("enddate", form.enddate);
-//                formMap.put("notes", form.notes);
-//                return formMap;
-//            }
-//        };
-
         Log.d("Request in add post", "printing request");
         System.out.println(jsonRequest);
         queue.add(jsonRequest);
         Log.d("Printing the queue", "queue");
         System.out.println(jsonRequest);
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.my, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
     }
 }
