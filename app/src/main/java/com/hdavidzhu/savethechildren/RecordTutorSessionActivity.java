bf3c6c0b3f1f1457864be7089181aac5a47c2939
Casey Alvarado <casey.alvarado@students.olin.edu>
2014-11-20 19:09:34 -0500

    Yayls Added a main activity, no errors. Need to implements tabs next

--- app/src/main/java/com/hdavidzhu/savethechildren/RecordTutorSessionActivity.java
+++ app/src/main/java/com/hdavidzhu/savethechildren/RecordTutorSessionActivity.java
@@ -9,7 +9,6 @@ import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.TimePicker;
-import com.example.android.supportv4.R;
 
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
@@ -39,108 +38,14 @@ import com.hdavidzhu.savethechildren.TabHelper.TabHelper;
 import org.json.JSONObject;
 
 public class RecordTutorSessionActivity extends Fragment {
-    private RecordTutorSessionActivity fragmentTabHost;
+    //private RecordTutorSessionActivity fragmentTabHost;
+//
+//
+//    StudentsDb studentsDb;
+//    private static final String TAG = "Couch";
+//
+//    View.OnClickListener submitButtonListener;
 
-
-    EditText nameEditText;
-    EditText notesEditText;
-    TimePicker timePicker;
-    DatePicker datePicker;
-    Button submitButton;
-
-    StudentsDb studentsDb;
-    private static final String TAG = "Couch";
-
-    View.OnClickListener submitButtonListener;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.record_tutor_session);
-
-//        TabHelper tabHelper = TabHelper.createInstance(context);
-//        tabHelper.setUp();
-        fragmentTabHost = (RecordTutorSessionActivity)findViewById(android.R.id.tabhost);
-        fragmentTabHost.setUp(this, getSupportFragmentManager(), R.id.realtabcontent);
-
-        context = getApplicationContext();
-
-        nameEditText = (EditText) findViewById(R.id.et_name);
-        notesEditText = (EditText) findViewById(R.id.et_notes);
-        timePicker = (TimePicker) findViewById(R.id.timePicker);
-        datePicker = (DatePicker) findViewById(R.id.datePicker);
-        submitButton = (Button) findViewById(R.id.bt_submit);
-
-        try {
-            studentsDb = new StudentsDb(context);
-            studentsDb.setUpDb();
-        } catch (CouchbaseLiteException c) {
-            Log.d("Couch", "Did not initiate database.");
-            return;
-        } catch (IOException e ) {
-            Log.d("Couch", "Did not initiate database.");
-            return;
-        }
-
-        submitButtonListener = new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Form form = new Form(
-                        nameEditText.getText().toString(),
-                        datePicker.getYear(),
-                        datePicker.getMonth(),
-                        datePicker.getDayOfMonth(),
-                        timePicker.getCurrentHour(),
-                        timePicker.getCurrentMinute(),
-                        notesEditText.getText().toString());
-
-                JSONObject jsonForm = form.javaToJSONObjectConverter();
-                postNewForm(context, jsonForm);
-
-                Document formDocument = studentsDb.database.createDocument();
-
-                try {
-                    @SuppressWarnings("unchecked")
-                    Map<String, Object> mapForm = form.toHashMapConverter();
-                    formDocument.putProperties(mapForm);
-                    Log.d (TAG, "Document written to database named " + studentsDb.dbName
-                            + " with ID = " + formDocument.getId());
-
-                    // save the ID of the new document
-                    String docID = formDocument.getId();
-                    // retrieve the document from the database
-                    Document retrievedDocument = studentsDb.database.getDocument(docID);
-                    // display the retrieved document
-                    Log.d(TAG, "retrievedDocument=" + String.valueOf(retrievedDocument.getProperties()));
-
-                    // Display number of files in document.
-                    Log.d(TAG, String.valueOf(studentsDb.database.getDocumentCount()));
-
-                } catch (CouchbaseLiteException e) {
-                    Log.e(TAG, "Cannot write document to database", e);
-                }
-            }
-        };
-
-        submitButton.setOnClickListener(submitButtonListener);
-
-        Query query = studentsDb.database.createAllDocumentsQuery();
-        query.setLimit(10);
-        query.setDescending(true);
-
-        try {
-            QueryEnumerator rowEnum = query.run();
-            for (Iterator<QueryRow> it = rowEnum;
-                 it.hasNext();) {
-                QueryRow row = it.next();
-                Log.d("Document ID:", row.getDocumentId());
-
-            }
-        } catch (CouchbaseLiteException e) {
-            e.printStackTrace();
-        }
-
-    }
 
     public void postNewForm(Context context, JSONObject jsonForm){
         // Instantiate the RequestQueue.
