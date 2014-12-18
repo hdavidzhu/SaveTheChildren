aa239cf4c272c3307df76896fc27ff31850f71f0
pmcote <pmcote1@gmail.com>
2014-12-17 22:39:16 -0500

    tna is error free

---
+++ app/src/main/java/com/hdavidzhu/savethechildren/TNAModelAdapter.java
@@ -0,0 +1,60 @@
+package com.hdavidzhu.savethechildren;
+
+import android.widget.ArrayAdapter;
+import java.util.ArrayList;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+/**
+ * Created by pmc on 12/17/14.
+ */
+public class TNAModelAdapter extends ArrayAdapter<TNAModel> {
+    private final Context context;
+    private final ArrayList<TNAModel> modelsArrayList;
+
+    public TNAModelAdapter(Context context, ArrayList<TNAModel> modelsArrayList) {
+
+        super(context, R.layout.tna_target, modelsArrayList);
+
+        this.context = context;
+        this.modelsArrayList = modelsArrayList;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+
+        // 1. Create inflater
+        LayoutInflater inflater = (LayoutInflater) context
+                .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+        // 2. Get rowView from inflater
+
+        View rowView = null;
+        if(!modelsArrayList.get(position).isGroupHeader){
+            rowView = inflater.inflate(R.layout.tna_target, parent, false);
+
+            // Get title & counter views from the rowView
+            TextView titleView = (TextView) rowView.findViewById(R.id.item_title);
+            TextView counterView = (TextView) rowView.findViewById(R.id.item_counter);
+
+            // Set the text for textView
+            titleView.setText(modelsArrayList.get(position).getTitle());
+            counterView.setText(modelsArrayList.get(position).getCounter());
+        }
+        else{
+            rowView = inflater.inflate(R.layout.tna_header, parent, false);
+            TextView titleView = (TextView) rowView.findViewById(R.id.header);
+            titleView.setText(modelsArrayList.get(position).getTitle());
+
+        }
+
+        // 5. return rowView
+        return rowView;
+    }
+}
